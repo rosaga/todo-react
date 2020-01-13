f028db89043ed92ac7722b132c488e4c8f701f74
richie <richie@richie-HP-EliteBook-820-G1>
2020-01-13 12:02:10 +0300

    final todo

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,16 +4,22 @@ import '../App.css';
 function TodoList(props) {
     const styles= {
         textAlign:'center',
-        fontSize: 16,
+        fontSize: 17,
         height: 'auto',
 
 
     }//when doing inline styles dont use a hypen use camel case
+    const completedstyle ={
+        fontStyle: "italic",
+        color: "#cdcdcd",
+        textDecoration: "line-through"
+    }
     return (
-        <div style = {styles}>
-            <div className = 'Todolist-items'>
-           <input type = 'checkbox' checked = {props.todo.completed} onChange = {()=> props.handleChange(props.todo.id)}/>{props.todo.text}
-           <hr></hr>
+        <div>
+            <div className = 'todo-item'>
+           <input type = 'checkbox' checked = {props.todo.completed} onChange = {()=> props.handleChange(props.todo.id)}/>
+            <p style= {props.todo.completed ? completedstyle : null}>{props.todo.text}</p><hr></hr>
+          
           
            </div>
         </div>
